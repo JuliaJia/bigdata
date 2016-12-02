1d56cd72dfa0d248befdbaf4a011d5ff937d7665
jialu <lu.jia@bluestonehk.com>
2016-12-02 22:55:34 +0800

    2016-12-02

--- hadoop/hadoop-hdfs/src/main/java/hadoop_senior/hadoop_hdfs/HdfsApp.java
+++ hadoop/hadoop-hdfs/src/main/java/hadoop_senior/hadoop_hdfs/HdfsApp.java
@@ -1,9 +1,13 @@
 package hadoop_senior.hadoop_hdfs;
 
+
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IOUtils;
@@ -13,7 +17,35 @@ public class HdfsApp {
 		System.setProperty("hadoop.home.dir", "F:\\study\\code\\hadoop-2.5.0\\hadoop-2.5.0");
 		FileSystem fileSystem = getFileSystem();
 		
-		String filename = "/usr/juliajia/mapreduce/wordcount/input/wc.input";
+		//String filename = "/usr/juliajia/mapreduce/wordcount/input/wc.input";
+		//readHdfsFile(filename,fileSystem);
+		String putPath = "/usr/juliajia/mapreduce/wordcount/input/put_wc.iput";
+		Path writePath = new Path(putPath);
+		
+		FSDataOutputStream outputStream = fileSystem.create(writePath);
+		
+		FileInputStream inputStream = new FileInputStream(new File("F:\\study\\code\\bigdata_github\\hadoop\\hadoop-hdfs\\src\\main\\resources\\test_upload"));
+		
+		
+		try {
+			IOUtils.copyBytes(inputStream, outputStream, 4096, false);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}finally{
+			IOUtils.closeStream(inputStream);
+			IOUtils.closeStream(outputStream);
+		}
+		
+	}
+	
+	public static FileSystem getFileSystem() throws IOException{
+		Configuration configuration = new Configuration();
+		
+		FileSystem fileSystem = FileSystem.get(configuration);
+		return fileSystem;
+	}
+	
+	public static void readHdfsFile(String filename,FileSystem fileSystem) throws IOException{
 		Path readPath = new Path(filename);
 		
 		FSDataInputStream inputStream = fileSystem.open(readPath);
@@ -26,14 +58,6 @@ public class HdfsApp {
 			IOUtils.closeStream(inputStream);
 		}
 		
-		
-	}
-	
-	public static FileSystem getFileSystem() throws IOException{
-		Configuration configuration = new Configuration();
-		
-		FileSystem fileSystem = FileSystem.get(configuration);
-		return fileSystem;
 	}
 
 }
